1b45793a6b9007729906f99d862cd7a6a5a877a1
finch0001 <michael_yubest@126.com>
2016-05-24 00:59:24 -0700

    add one file:mail2.py

---
+++ mail2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
+
+sender = 'michael_yubest@126.com';
+#receivers = '2276939579@qq.com';
+receivers = '2276939579@qq.com';
+
+message = """From: From Michael Yu <le_wan@126.com>
+To: To Finch(By le wan 2016/03/22/12:53) 2276939579@qq.com
+Subject: SMTP e-mail for a test. This email write by Python. This is a test e-mail message.
+"""
+
+for i in range(3):
+    try:
+        smtpObj = smtplib.SMTP('smtp.126.com')
+        smtpObj.login("michael_yubest@126.com", "michael_yuwhjxys")
+        smtpObj.sendmail(sender, receivers, message)
+        print "Successfully sent email"
+    except smtplib.SMTPException:
+        print "Error: unable to send email"
